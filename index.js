399edf8607caaca5ab53906d166e54ea2c2a5ee4
이상훈 <dl57934@naver.com>
2020-03-01 02:07:42 +0900

    complete input value and click send BUtton

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const name;
+const inputBox = document.getElementById("input");
+const inputBoxElements = inputBox.getElementsByTagName("div");
+
+let inputTag;
+
+for (let i = 0; i < inputBoxElements.length; i++) {
+  if (inputBoxElements[i].id == "input") {
+    inputTag = inputBoxElements[i];
+    break;
+  }
+}
+
+inputTag.innerText = "test2";
+
+const buttonBox = document.getElementById("send-button");
+const buttonElement = buttonBox.getElementsByTagName("button");
+buttonElement[0].click();
