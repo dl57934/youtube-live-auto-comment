d628c477e56f01faa3059bd810412731abc37e68
이상훈 <dl57934@naver.com>
2020-03-01 19:10:25 +0900

    atuo write comment and send

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const inputBox = document.getElementById("input");
-const inputBoxElements = inputBox.getElementsByTagName("div");
-
-let inputTag;
-
-for (let i = 0; i < inputBoxElements.length; i++) {
-  if (inputBoxElements[i].id == "input") {
-    inputTag = inputBoxElements[i];
-    break;
-  }
+function autoCommentWrite() {
+  let inputTag = $("#input > #input");
+  inputTag.innerText = "test2";
+  console.log("work");
+  setTimeout(function() {
+    let buttonBox = document.getElementById("send-button");
+    let buttonElement = buttonBox.getElementsByTagName("button");
+    buttonElement[0].click();
+    clearTimeout();
+  }, 2000);
 }
 
-inputTag.innerText = "test2";
-
-const buttonBox = document.getElementById("send-button");
-const buttonElement = buttonBox.getElementsByTagName("button");
-buttonElement[0].click();
+setTimeout(autoCommentWrite(), 3000);
